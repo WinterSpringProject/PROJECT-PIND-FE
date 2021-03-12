300761b8147e8b689bbc7e73e6163c195dedd670
hysimok <jeonghy95@gmail.com>
2021-03-12 14:43:06 +0900

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CardViewTemplate from './components/CardView/CardViewTemplate';
 import CardViewList from './components/CardView/CardViewList';
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
 import mockCard from './components/CardView/CardViewMock';
 
@@ -8,6 +9,7 @@ const App = () => {
   const [cards, setCards]=useState(()=>(mockCard))
   return (
     <div>
+      <NavBar />
       <CardViewTemplate>
         <CardViewList cards={cards}/>
       </CardViewTemplate>
