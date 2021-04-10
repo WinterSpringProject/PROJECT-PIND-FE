8543353f4aae2a3e85917130955473f3aca72b62
Seoungwoo Baek <br1992ay@gmail.com>
2021-04-10 17:59:20 +0900

    Revert "FIX: delete hroku"

---
+++ src/components/TopCategoryList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TopCategoryList = () => {
+	return (
+		<div>
+			<div>
+				<div>Left Button</div>
+				<div>TopCategoryListItem</div>
+				<div>Right Button</div>
+			</div>
+			<div>Search Bar</div>
+			<hr/>
+		</div>
+	);
+};
+
+export default TopCategoryList;
