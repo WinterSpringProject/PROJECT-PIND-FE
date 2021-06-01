7ae8e92656fb3158f2d9e090047323f0da5c89b3
Seoungwoo Baek <br1992ay@gmail.com>
2021-06-01 18:29:25 +0900

    FIX: #40 CSS 오류 수정
    새 프로젝트 부분 CSS 오류 수정

--- src/components/Project/ProjectDetail.js
+++ src/components/Project/ProjectDetail.js
@@ -24,46 +24,56 @@ const ProjectDetail = ({ match }) => {
 			<div>Loading..</div>
 		)
 	}
+
+	const onDelete = () => {
+		axios.delete(url);
+	}
+
 	const imgUrl = project.file ? `${SERVER_IP}/file?fileName=${project.file.url}`: logo;
 	const startDate = project.startDate.substring(0, 10)
 	return (
-		<div className="ProjectDetail-wrapper">
-			<div className="ProjectDetail-thumbnail">
-				<img className="ProjectDetail-thumbnail img" src={imgUrl} alt="project thumbnail"/>
+		<div>
+			<div className="ProjectDetail-wrapper">
+				<div className="ProjectDetail-thumbnail">
+					<img className="ProjectDetail-thumbnail img" src={imgUrl} alt="project thumbnail"/>
+				</div>
+				<div className="ProjectDetail-overview">
+					<div className="ProjectDetail-top">
+						<div className="ProjectDetail-name"> {project.title} </div>
+						<div className="ProjectDetail-underline"/>
+					</div>
+					<div className="ProjectDetail-info">
+						<div className="ProjectDetail-theme1">{"프로젝트 정보"}</div>
+						<div className="ProjectDetail-theme2">
+							<div className="ProjectDetail-category">
+								{project.subject.subject1} | {project.subject.subject2}
+							</div>
+							<div className="ProjectDetail-description">{project.description}</div>
+							<div className="ProjectDetail-content">{project.content}</div>
+							<div className="ProjectDetail-middle">
+								<div>{project.participateNum} | {project.maxParticipateNum}</div>
+							</div>
+							<div className="ProjectDetail-region">
+								<div>{project.region.region1}</div>
+								<div>{project.region.region2}</div>
+								<div>{project.region.region3}</div>
+							</div>
+							<div className="ProjectDetail-date">{startDate}</div>
+						</div>
+					</div>
+					<div>
+						<div>{"리더 프로필"}</div>
+						<div className="ProjectDetail-user">
+							<img className="ProjectDetail-user userimg" src={logo} alt="" width="20"/>
+							<div className="ProjectDetail-user userinfo">
+								<div>{project.leader.name}</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="ProjectDetail-overview">
-				<div className="ProjectDetail-top">
-					<div className="ProjectDetail-name"> {project.title} </div>
-					<div className="ProjectDetail-underline"/>
-				</div>
-				<div className="ProjectDetail-info">
-					<div className="ProjectDetail-theme1">{"프로젝트 정보"}</div>
-					<div className="ProjectDetail-theme2">
-						<div className="ProjectDetail-category">
-							{project.subject.subject1} | {project.subject.subject2}
-						</div>
-						<div className="ProjectDetail-description">{project.description}</div>
-						<div className="ProjectDetail-content">{project.content}</div>
-						<div className="ProjectDetail-middle">
-							<div>{project.participateNum} | {project.maxParticipateNum}</div>
-						</div>
-						<div className="ProjectDetail-region">
-							<div>{project.region.region1}</div>
-							<div>{project.region.region2}</div>
-							<div>{project.region.region3}</div>
-						</div>
-						<div className="ProjectDetail-date">{startDate}</div>
-					</div>
-				</div>
-				<div>
-					<div>{"리더 프로필"}</div>
-					<div className="ProjectDetail-user">
-						<img className="ProjectDetail-user userimg" src={logo} alt="" width="20"/>
-						<div className="ProjectDetail-user userinfo">
-							<div>{project.leader.name}</div>
-						</div>
-					</div>
-				</div>
+			<div>
+				<button onClick={onDelete}>삭제</button>
 			</div>
 		</div>
 	);
