1acbebc45cc2f6e5f6721bc380701a9bdd3f21f1
Seoungwoo Baek <br1992ay@gmail.com>
2021-06-01 20:09:13 +0900

    FIX: Fixing MenuList component
    네비게이션 바 새프로젝트 링크 부분 수정

--- src/components/NavBar/MenuList.js
+++ src/components/NavBar/MenuList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 const MenuItem = ({ selected, menu, selectMenu }) => {
@@ -12,7 +13,9 @@ const MenuItem = ({ selected, menu, selectMenu }) => {
 		<div
 			onClick={onClickMenu}
 			className={`NavBar-click ${selected === name ? 'selected' : ''}`}>
-			{text}
+			{text === '새프로젝트' && <Link to="/newproject" className="NavBar-link">{text}</Link>}
+			{text === '주제별' && <Link to="/" className="NavBar-link">{text}</Link>}
+			{text === '지역별' && text}
 		</div>
 	);
 };
