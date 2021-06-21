b1496d5fe587bd027ff52f1aefd8a883a9c14d1e
Seoungwoo Baek <br1992ay@gmail.com>
2021-06-22 03:03:38 +0900

    FEAT: #8 Create Pagination
    페이지네이션 구현

--- src/components/CardView/CardViewList.js
+++ src/components/CardView/CardViewList.js
@@ -6,6 +6,13 @@ import { useSelector } from 'react-redux';
 import { SERVER_IP } from '../../constants/serverInfo';
 
 const CardViewList = () => {
+	const [pages, setResponse] = useState();
+	const [inputPage, setInputPage] = useState("");
+	const [pageNum, setPage] = useState(
+		{
+			page:""
+		}
+	);
 	const [projects, setProject] = useState(null);
 	const selectedCategory = useSelector(state => state.category.selected);
 	//const SERVER_IP2 = 'http://127.0.0.1:8080';
@@ -14,6 +21,7 @@ const CardViewList = () => {
 		const fetchData = async () => {
 			try {
 				const response = await axios.get(`${SERVER_IP}/project/list`);
+				setResponse(response.data);
 				setProject(response.data.dtoList);
 				const filteredProject = selectedCategory ?
 							response.data.dtoList.filter(data => data.subject.subject1 === selectedCategory) : response.data.dtoList;
@@ -24,17 +32,46 @@ const CardViewList = () => {
 		fetchData();
 	}, [selectedCategory]);
 
-	//useEffect(() => {
-	//	axios.get(`${SERVER_IP}/project/list`)
-	//	.then( summonerData => { console.log(summonerData) } )
-	//	.catch( error => console.log("Data가 없습니다.") );
-	//}, [])
+	const onChangePage = (e) => {
+		setInputPage(e.target.value);
+		const {value, name} = e.target;
+		setPage({
+			...pageNum,
+			[name]: value
+		});
+	}
 
 	if (!projects) {
 		return (
 			<div>Loading..</div>
 		)
 	}
+
+	const fetchPageData = async () => {
+		try {
+			const response = await axios.get(`${SERVER_IP}/project/list`, pageNum);
+			setProject(response.data.dtoList);
+			const filteredProject = selectedCategory ?
+						response.data.dtoList.filter(data => data.subject.subject1 === selectedCategory) : response.data.dtoList;
+			setProject(filteredProject);
+		}
+		catch (e) { console.log(e); }
+	};
+
+	const handlePage = () => {
+		fetchPageData();
+	}
+
+	const handlePageNumClick = (e) => {
+		const {value, name} = e.target;
+		setPage({
+			...pageNum,
+			[name]: value
+		});
+		fetchPageData();
+	}
+
+
 	return (
 		<div className="CardViewTemplate-wrapper">
 			<div className="CardViewList-wrapper">
@@ -45,6 +82,20 @@ const CardViewList = () => {
 				/>
 			))}
 			</div>
+				<div>
+					{pages.pageList.map((number) => {
+						return(<button key={number} onClick={handlePageNumClick} value={number} name="page">{number}</button>);
+					})}
+				</div>
+				<div>
+					<input className = "Page-input"
+											type="text"
+											name="page"
+											placeholder="page"
+											value={inputPage}
+											onChange={onChangePage}/>
+					<button onClick={handlePage}>이동</button>
+				</div>
 		</div>
 	);
 };
--- src/components/Project/PopupDelete.js
+++ src/components/Project/PopupDelete.js
@@ -2,13 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import './PopupDelete.scss'
 
+export const getCookie = (cookie_name) => {
+	let x, y;
+	let val = document.cookie.split(';');
+
+	for (var i = 0; i < val.length; i++) {
+		x = val[i].substr(0, val[i].indexOf('='));
+		y = val[i].substr(val[i].indexOf('=') + 1);
+		x = x.replace(/^\s+|\s+$/g, '');
+		if (x === cookie_name) {
+			return unescape(y);
+		}
+	}
+}
+
+
 const PopupDelete = (props) => {
-	const url = props.url
-	const { onClose } = props
+	const url = props.url;
+	const { onClose } = props;
+	const auth = getCookie('auth');
+	const authHeader = {
+		headers: { Authorization: `Baerer ${auth}` }
+	}
 
 	const onDelete = async () => {
 		try {
-			await axios.delete(url);
+			await axios.delete(url, authHeader);
 			window.location = '/';
 		}
 		catch (e) {console.log(e);}
